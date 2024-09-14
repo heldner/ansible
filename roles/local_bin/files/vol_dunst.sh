67c65ea2ffd5063ce3ca3b26bc3ac58a5788ef32
Stanislav Korolev <stanislav.korolev87@mail.ru>
2024-09-14 19:43:15 +0300

    show muted state

--- roles/local_bin/files/vol_dunst.sh
+++ roles/local_bin/files/vol_dunst.sh
@@ -1,26 +1,35 @@
 #!/bin/sh
 
 case "$1" in
-    up)
-        wpctl set-volume @DEFAULT_AUDIO_SINK@ 2%+ --limit 1 ;;
-    down)
-        wpctl set-volume @DEFAULT_AUDIO_SINK@ 2%- --limit 1 ;;
-    *)
-        exit 0 ;;
+up)
+    wpctl set-volume @DEFAULT_AUDIO_SINK@ 2%+ --limit 1
+    ;;
+down)
+    wpctl set-volume @DEFAULT_AUDIO_SINK@ 2%- --limit 1
+    ;;
+toggle)
+    wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
+    ;;
+*) exit 0 ;;
 esac
 
-volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print ($2*100)}')
+src_volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@)
+volume=$(echo "$src_volume" | awk '{print ($2*100)}')
 
 if [ "$volume" -le 33 ]; then
-    level='low'
+    icon=audio-volume-low-symbolic.svg
 elif [ "$volume" -le 66 ]; then
-    level='medium'
+    icon=audio-volume-medium-symbolic.svg
 else
-    level='high'
+    icon=audio-volume-high-symbolic.svg
+fi
+
+if echo "$src_volume" | grep -q '[MUTED]'; then
+    icon=audio-volume-muted-symbolic.svg
 fi
 
 dunstify -a "changeVolume" \
     -u low \
-    -i "/usr/share/icons/Faba/symbolic/status/audio-volume-$level-symbolic.svg" \
+    -i "/usr/share/icons/Faba/symbolic/status/$icon" \
     -h string:x-dunst-stack-tag:"vol" \
-    -h int:value:"$volume" "Volume: ${volume}%"
+    -h int:value:"$volume" "Volume: $volume%"
