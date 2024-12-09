520c9d99972a72d8c85d2b0c8eefeea1b3809e8e
Stanislav Korolev <stan.korolev@mail.ru>
2024-12-09 21:01:54 +0300

    add multiple ipset tables

--- roles/iptables/files/ipset-reload.sh
+++ roles/iptables/files/ipset-reload.sh
@@ -1,34 +1,18 @@
 #!/bin/sh
 
-IPSET_RULES=/etc/ipset.tables
-TMP_IPSET_RULES=/tmp/ipset.tables.tmp
+set -e
+
+IPSET_DIR=/etc/ipset/portsentry.tables
 
 reload() {
-    set -e
-    awk '/^create / {sub(/_tmp/,""); print $0}' $IPSET_RULES | \
-    while read ip_set; do
+    file="$1"
+    awk '/^create / {sub(/_tmp/,""); print $0}' "$file" | \
+    while read -r ip_set; do
         ipset -exist $ip_set
     done
-    ipset -exist restore < $IPSET_RULES
-
+    ipset -exist restore < "$file"
 }
 
-main() {
-    if reload; then
-        cp $IPSET_RULES $TMP_IPSET_RULES
-    fi
-}
-
-
-if [ "$1" = '--force-reload' ];then
-  rm $TMP_IPSET_RULES
-fi
-
-if [ ! -f $TMP_IPSET_RULES  ]; then
-    main
-else
-    diff $IPSET_RULES $TMP_IPSET_RULES > /dev/null
-    if [ $? -eq 1 ]; then
-        main
-    fi
-fi
+for rules in $IPSET_DIR; do
+    reload "$rules"
+done
