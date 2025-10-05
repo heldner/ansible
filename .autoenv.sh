98b2b94be55c7cbb000ac6d2e4c9d20f826f0cb2
Stanislav Korolev <stan.korolev@mail.ru>
2025-10-05 11:27:23 +0300

    use uv for venv management

--- .autoenv.sh
+++ .autoenv.sh
@@ -1,3 +1,3 @@
-autostash ANSIBLE_VAULT_PASSWORD_FILE="$(pwd)/vault-pass.sh"
+autostash ANSIBLE_VAULT_PASSWORD_FILE="$(pwd)/vault-pass"
 autostash ARA_API_CLIENT="http"
 autostash ARA_API_SERVER="http://127.0.0.1:8000"
